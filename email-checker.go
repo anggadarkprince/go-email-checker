12b005abeea9dbec16f61c25fd0bf7e00576fed9
Angga Ari Wijaya <anggadarkprince@gmail.com>
2025-04-05 21:39:46 +0700

    BREAKING CHANGES: update to v2

--- email-checker.go
+++ email-checker.go
@@ -5,9 +5,11 @@ import (
 	"strings"
 )
 
-func CheckMail(email string) bool {
-	if (strings.HasSuffix(email, "@exmaple.com") || strings.HasSuffix(email, "@test.com")) {
-		return false
+func CheckMail(email string, isStrict bool) bool {
+	if (isStrict) {
+		if (strings.HasSuffix(email, "@exmaple.com") || strings.HasSuffix(email, "@test.com")) {
+			return false
+		}
 	}
 	_, err := mail.ParseAddress(email)
     return err == nil
